0f222a525195899d9765d84944d6e39dce97c192
bvokus <32828665+bvokus@users.noreply.github.com>
2017-10-17 03:38:01 -0400

    updated weapon factory

--- WeaponFactory.cpp
+++ WeaponFactory.cpp
@@ -9,6 +9,9 @@
 #include "WeaponFactory.h"
 #include "CommonSword.h" 
 #include "CommonSpear.h"
+#include "SimpleAxe.h"
+#include "CrazyRandomSword.h"
+#include "MachineGun.h"
 
 WeaponFactory* WeaponFactory::instance = NULL;
 
@@ -26,6 +29,15 @@ Weapon * WeaponFactory::getWeapon(std::string name) {
     
     if (name.compare("spear") == 0) {
         return new CommonSpear();
+    }
+    if (name.compare("axe") == 0) {
+        return new SimpleAxe();
+    }
+    if (name.compare("sword") == 0) {
+        return new CrazyRandomSword();
+    }
+    if (name.compare("gun") == 0) {
+        return new MachineGun();
     }
 
     throw "Invalid weapon";
